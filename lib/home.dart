20d34f34a521ebcea96d6403021b05b3db9ed125
Quentin Newton <quentinnewton@gmail.com>
2022-11-23 16:48:01 +0100

    Displaying data from reddit

--- lib/home.dart
+++ lib/home.dart
@@ -15,6 +15,7 @@ class Home extends StatelessWidget {
         )..add(LoadPostEvent()),
       child: Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.deepOrange,
           title: const Text('Brawn Practice Task by Quentin'),
         ),
         body: BlocBuilder<PostBloc, PostState>(
@@ -29,7 +30,106 @@ class Home extends StatelessWidget {
                   itemCount: state.posts.data?.children?.length,
                   itemBuilder: (_, index) {
                     var post = state.posts.data?.children?[index];
-                    return Text(post!.data!.author.toString());
+                    return Container(
+                      child: Card(
+                        color: Colors.white,
+                        elevation: 2,
+                        margin: EdgeInsets.symmetric(vertical: 10),
+                        child: Column(
+                          children: [
+
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    decoration: BoxDecoration(
+                                        borderRadius:
+                                        BorderRadius.circular(20),
+                                        color: Colors.deepOrange),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(5),
+                                      child: Text(post!.data!.linkFlairText
+                                          .toString(), style: TextStyle(color: Colors.white),),
+                                    ),
+                                  ),
+                                  const SizedBox(width: 20),
+                                  Flexible(
+                                      child: Text(
+                                        post.data!.title.toString(),
+                                        style: const TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w500),
+                                      ))
+                                ],
+                              ),
+                            ),
+                            const SizedBox(height: 20),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(post.data!.selftext.toString()),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  const Icon(
+                                    Icons.chat_bubble_outline,
+                                    color: Colors.black54,
+                                  ),
+                                  const SizedBox(width: 5),
+                                  Padding(
+                                    padding:
+                                    const EdgeInsets.only(bottom: 5.0),
+                                    child: Text(
+                                      '${post.data!.numComments} comments',
+                                      style: const TextStyle(
+                                          color: Colors.black54),
+                                    ),
+                                  ),
+                                  const SizedBox(width: 20),
+                                  const Icon(
+                                    Icons.share,
+                                    color: Colors.black54,
+                                  ),
+                                  const SizedBox(width: 5),
+                                  const Padding(
+                                    padding: EdgeInsets.only(bottom: 5.0),
+                                    child: Text(
+                                      ' Share',
+                                      style: TextStyle(color: Colors.black54),
+                                    ),
+                                  ),
+                                  const SizedBox(width: 20),
+                                  const Icon(
+                                    Icons.bookmark_border,
+                                    color: Colors.black54,
+                                  ),
+                                  const SizedBox(width: 5),
+                                  const Padding(
+                                    padding: EdgeInsets.only(bottom: 5.0),
+                                    child: Text(
+                                      ' Save',
+                                      style: TextStyle(color: Colors.black54),
+                                    ),
+                                  ),
+                                  const Spacer(),
+                                  Column(
+                                    children: [
+                                      const Icon(Icons.arrow_upward_rounded),
+                                      Text(post.data!.ups.toString()),
+                                      const Icon(
+                                          Icons.arrow_downward_rounded),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
                   });
             }
             if (state is PostErrorState) {
